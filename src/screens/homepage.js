6a54408e5f55434412f86839ea9f8cb18c0ed497
Nick Spanos <nickspanosf1@gmail.com>
2023-09-05 18:35:49 +0300

    navbar added

---
+++ src/screens/homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MainNavbar from '../components/navbar'
+
+
+function Homepage(){
+    return(
+        <div>
+        <MainNavbar></MainNavbar>
+        </div>
+    );
+}
+
+export default Homepage;
