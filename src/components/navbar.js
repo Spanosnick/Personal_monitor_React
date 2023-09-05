ed1b4de15d87b15d4b14226d67fa3b04477d8862
Nick Spanos <nickspanosf1@gmail.com>
2023-09-05 18:41:11 +0300

    navbar work now

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,23 +8,13 @@ function MainNavbar() {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
+                        <Nav.Link href="#home">Αρχική</Nav.Link>
+                        <Nav.Link href="#infos">Πληροφορίες</Nav.Link>
+                        <Nav.Link href="#message">Μηνύματα</Nav.Link>
+                        <Nav.Link href="#profile">Προφίλ</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
